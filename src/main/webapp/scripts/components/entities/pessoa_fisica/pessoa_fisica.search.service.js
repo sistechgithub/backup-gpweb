25990077c75a692c4c8ad3f4a5476e83d735a682
Levy Moreira <levymoreira.ce@gmail.com>
2016-02-07 13:20:15 -0300

    Remove cruds pessoa, pessoa fisica e pessoa juridica

--- src/main/webapp/scripts/components/entities/pessoa_fisica/pessoa_fisica.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('gpApp')
-    .factory('Pessoa_fisicaSearch', function ($resource) {
-        return $resource('api/_search/pessoa_fisicas/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
