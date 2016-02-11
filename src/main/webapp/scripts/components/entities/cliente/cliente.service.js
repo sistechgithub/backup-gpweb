d9dfa272db347a939b8fcc5dc83082cc2dcd1836
jacksondeveloper <jackson.jack.sth@gmail.com>
2016-02-11 11:18:26 -0300

    Correções no cadatro de Cliente

--- src/main/webapp/scripts/components/entities/cliente/cliente.service.js
+++ src/main/webapp/scripts/components/entities/cliente/cliente.service.js
@@ -6,11 +6,23 @@ angular.module('gpApp')
             'query': { method: 'GET', isArray: true},
             'get': {
                 method: 'GET',
-                transformResponse: function (data) {
+                transformResponse: function (data) {                	
                     data = angular.fromJson(data);
+                    data.pessoaFisica.dataNascimento = DateUtils.convertLocaleDateFromServer(data.pessoaFisica.dataNascimento);
                     return data;
                 }
             },
-            'update': { method:'PUT' }
+            'update': {
+                method: 'PUT',
+                transformRequest: function (data) {                	
+                    return angular.toJson(data);
+                }
+            },
+            'save': {
+                method: 'POST',
+                transformRequest: function (data) {                	
+                    return angular.toJson(data);
+                }
+            }
         });
     });
