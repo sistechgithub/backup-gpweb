84920ef696c84e58ee2bb5dc0ba281fe4b556378
jacksondeveloper <jackson.jack.sth@gmail.com>
2016-03-09 08:39:48 -0300

    implementando pesquisa no cadastro de grupos

--- src/main/java/com/sth/gp/repository/GrupoRepository.java
+++ src/main/java/com/sth/gp/repository/GrupoRepository.java
@@ -1,14 +1,17 @@
 package com.sth.gp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.sth.gp.domain.Grupo;
-
-import org.springframework.data.jpa.repository.*;
-
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the Grupo entity.
  */
 public interface GrupoRepository extends JpaRepository<Grupo,Long> {
 	
+	Page<Grupo> findById(Long id, Pageable pageable);
+	Page<Grupo> findByNmGrupoStartingWithOrderByNmGrupoAsc(String descricao, Pageable pageable);
+		
 }
